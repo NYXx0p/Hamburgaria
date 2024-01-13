168af1f2cc0e03ec3e3202aaa1fadad1095b2bfc
NYXx0p <153073613+NYXx0p@users.noreply.github.com>
2024-01-13 21:56:56 +0000

    Inventory

--- src/main/java/com/school/sample/MainApp.java
+++ src/main/java/com/school/sample/MainApp.java
@@ -10,7 +10,7 @@ public class MainApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         //1º PASSO - O método Start associa este Layout a um objeto da classe Parent.
-        Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Principal1.fxml"));
         //2º PASSO - Definição do titulo para a Stage
         primaryStage.setTitle("Vai tomar no Cu");
         //3º PASSO - Associação do Layout do Parent à Sceme e esta à Stage
--- src/main/java/com/school/sample/PrincipalController.java
+++ src/main/java/com/school/sample/PrincipalController.java
@@ -6,15 +6,34 @@ import javafx.scene.input.MouseEvent;
 
 public class PrincipalController {
     @FXML
+    private Button btnInventario;
+    @FXML
+    private Button btnImportar;
+    @FXML
+    private Button btnAdicionar;
+    @FXML
+    private Button btnEditar;
+    @FXML
+    private Button btnLimpar;
+    @FXML
+    private Button btnEliminar;
+    @FXML
     private Button btnFuncionario;
     @FXML
-    private Button btnSair;
+    private Button btnVoltar;
     @FXML
     private Button btnMenu;
     @FXML
     private Button btnReserva;
     @FXML
     private Button btnCliente;
+
+    public void btnInventarioEntered(MouseEvent mouseEvent) {
+        btnInventario.setStyle("-fx-background-color: white; -fx-text-fill: #000;");
+    }
+    public void btnInventarioExited(MouseEvent mouseEvent) {
+        btnInventario.setStyle("-fx-background-color: transparent; -fx-border-color: #fff;");
+    }
 
     public void btnMenuEntered(MouseEvent mouseEvent) {
         btnMenu.setStyle("-fx-background-color: white; -fx-text-fill: #000;");
@@ -49,11 +68,51 @@ public class PrincipalController {
         btnFuncionario.setStyle("-fx-background-color: transparent; -fx-border-color: #fff;");
     }
 
-    public void btnSairEntered(MouseEvent mouseEvent) {
-        btnSair.setStyle("-fx-background-color: white; -fx-text-fill: #000;");
+    public void btnVoltarEntered(MouseEvent mouseEvent) {
+        btnVoltar.setStyle("-fx-background-color: white; -fx-text-fill: #000;");
     }
 
-    public void btnSairExited(MouseEvent mouseEvent) {
-        btnSair.setStyle("-fx-background-color: transparent; -fx-border-color: #fff;");
+    public void btnVoltarExited(MouseEvent mouseEvent) {
+        btnVoltar.setStyle("-fx-background-color: transparent; -fx-border-color: #fff;");
     }
+    public void btnImportarEntered(MouseEvent mouseEvent) {
+        btnImportar.setStyle("-fx-background-color: #c0c2c1; -fx-text-fill: #000; -fx-border-radius: 4px; -fx-border-color: #c0c2c1;");
+    }
+
+    public void btnImportarExited(MouseEvent mouseEvent) {
+        btnImportar.setStyle("-fx-background-color: #313338; -fx-background-radius: 4px; -fx-text-fill: #fff;");
+    }
+
+    public void btnAdicionarEntered(MouseEvent mouseEvent) {
+        btnAdicionar.setStyle("-fx-background-color: #c0c2c1; -fx-text-fill: #000; -fx-border-radius: 4px; -fx-border-color: #c0c2c1;");
+    }
+
+    public void btnAdicionarExited(MouseEvent mouseEvent) {
+        btnAdicionar.setStyle("-fx-background-color: #313338; -fx-background-radius: 4px; -fx-text-fill: #fff;");
+    }
+
+    public void btnEditarEntered(MouseEvent mouseEvent) {
+        btnEditar.setStyle("-fx-background-color: #c0c2c1; -fx-text-fill: #000; -fx-border-radius: 4px; -fx-border-color: #c0c2c1;");
+    }
+
+    public void btnEditarExited(MouseEvent mouseEvent) {
+        btnEditar.setStyle("-fx-background-color: #313338; -fx-background-radius: 4px; -fx-text-fill: #fff;");
+    }
+
+    public void btnLimparEntered(MouseEvent mouseEvent) {
+        btnLimpar.setStyle("-fx-background-color: #c0c2c1; -fx-text-fill: #000; -fx-border-radius: 4px; -fx-border-color: #c0c2c1;");
+    }
+
+    public void btnLimparExited(MouseEvent mouseEvent) {
+        btnLimpar.setStyle("-fx-background-color: #313338; -fx-background-radius: 4px; -fx-text-fill: #fff;");
+    }
+
+    public void btnEliminarEntered(MouseEvent mouseEvent) {
+        btnEliminar.setStyle("-fx-background-color: #c0c2c1; -fx-text-fill: #000; -fx-border-radius: 4px; -fx-border-color: #c0c2c1;");
+    }
+
+    public void btnEliminarExited(MouseEvent mouseEvent) {
+        btnEliminar.setStyle("-fx-background-color: #313338; -fx-background-radius: 4px; -fx-text-fill: #fff;");
+    }
+
 }
